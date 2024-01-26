3dd5b10288b516a3ce1ea83d4721f60a138ca80c
AnimeshKatigar <katigaranimesh@gmail.com>
2024-01-26 21:21:28 +0530

    Navbar: Added active pathname validation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,17 @@
 import { useState } from "react";
 import { Spin as Hamburger } from "hamburger-react";
 import { motion } from "framer-motion";
-import logoWhite from "../../public/assets/logos/LogoWhiteBg.png"
-import logoBlack from "../../public/assets/logos/LogoBlackBg.png"
+import { usePathname } from "next/navigation";
+import logoWhite from "../../public/assets/logos/LogoWhiteBg.png";
+import logoBlack from "../../public/assets/logos/LogoBlackBg.png";
 import Image from "next/image";
 import Link from "next/link";
+import BagIcon from "../../public/assets/icons/BagWhite.svg";
+import CartMenu from "./Layovers/CartMenu";
 
 const Navbar = () => {
-  const [burgerMenuToggle, setBurgerMenuToggle] = useState(true);
+  const pathname = usePathname();
+  const [burgerMenuToggle, setBurgerMenuToggle] = useState(false);
   const navbarVariants = {
     visible: {
       y: 0,
@@ -17,30 +21,58 @@ const Navbar = () => {
     },
     hidden: { y: "-100%", opacity: 0 },
   };
+  const routes = [
+    { label: "Shop", path: "/shop" },
+    { label: "Collections", path: "/collections" },
+  ];
+
+  const checkActiveRoute = (path) => {
+    return pathname.includes(path);
+  };
 
   return (
-    <motion.div
+    <motion.nav
       // animate="visible"
       // variants={navbarVariants}
       // initial="hidden"
-      className={`${
-        // bg
-        //   ? "sm:bg-black navbar-darkBg"
-        //   : scrollPosition >= windowHeight - 30 ?
-        "navbar-darkBg"
-        // : "navbar-bg"
-      } px-[3rem] flex items-center sm:py-[1rem] sm:max-lg:py-3 max-lg:px-[1.25rem] max-sm:py-1 max-sm:h-[56px] fixed w-screen z-[5] top-0 font-GothamMedium h-[70px]`}
+      className={`md:justify-between navbar-darkBg px-[3rem] flex items-center sm:py-[1rem] sm:max-lg:py-3 max-lg:px-[1.25rem] max-sm:py-1 max-sm:h-[56px] fixed w-screen z-[5] top-0 font-GothamMedium h-[70px]`}
     >
-      <Image src={logoBlack} alt="logo-dark" className="h-16 w-16"/>
-      <Link href="/shop/all" className="text-white border-b">SHOP</Link>
-      <Hamburger
-        direction="left"
-        toggled={burgerMenuToggle}
-        toggle={setBurgerMenuToggle}
-        color="white"
-        hideOutline={true}
-      />
-    </motion.div>
+      <div className="md:hidden">
+        <Hamburger
+          direction="left"
+          toggled={burgerMenuToggle}
+          toggle={setBurgerMenuToggle}
+          color="white"
+          hideOutline={true}
+        />
+      </div>
+      <Link href="/">
+        <Image src={logoBlack} alt="logo-dark" className="h-16 w-16" />
+      </Link>
+      <div className="hidden md:flex items-center gap-x-3 h-full">
+        {routes.map((val) => (
+          <Link
+            key={val.path}
+            href={val.path}
+            className={`text-white ${checkActiveRoute(val.path) && "border-b"}`}
+          >
+            {val.label}
+          </Link>
+        ))}
+      </div>
+      <div>
+        <CartMenu
+          triggerComponent={() => (
+            <Image
+              src={BagIcon}
+              className="hover:scale-110 transition-all duration-200 cursor-pointer"
+              alt="cart"
+            />
+          )}
+          closeComponent={() => <div>CLOSE THIS COMPONENT</div>}
+        />
+      </div>
+    </motion.nav>
   );
 };
 
